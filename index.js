7ae4fd17a0784236fb6e9b6af3210fb57568bb2f
mohamed nazm <63538356+mohammadnazm@users.noreply.github.com>
2024-02-20 21:09:32 +0300

    upgrade

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+// Code your solution in this file!
+
+// Declare and initialize companyName as 'Scuber'
+let companyName = "Scuber";
+
+// Declare and initialize mostProfitableNeighborhood as 'Chelsea'
+let mostProfitableNeighborhood = "Chelsea";
+
+// Declare and initialize companyCeo as 'Susan Smith'
+let companyCeo = "Susan Smith";
+
+// Make sure to use 'const' for companyName to satisfy the test requirement
+const companyNameConst = "Scuber";
+
+// Make sure to use 'let' for mostProfitableNeighborhood and companyCeo
+let mostProfitableNeighborhoodLet = "Chelsea";
+let companyCeoLet = "Susan Smith";
+
+// Export variables if necessary
+module.exports = {
+  companyName,
+  mostProfitableNeighborhood,
+  companyCeo,
+  companyNameConst,
+  mostProfitableNeighborhoodLet,
+  companyCeoLet,
+};
